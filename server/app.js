f658814548a2792d47be77e413cae7263a4fea46
Gojou-Satoru-8 <me@ankushbhowmik.com>
2024-11-09 12:36:32 +0530

    Introduced security middelewares in app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+// const xss = require("xss-clean");  // Deprecated - need to search for alternative
+const hpp = require("hpp");
 
 const User = require("./models/User");
 const authRouter = require("./routes/authRouter");
@@ -11,11 +16,37 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 const { DB_URI, DB_PASSWORD, SESSION_STORE_SECRET, NODE_ENV } = process.env;
-
 const DB = DB_URI.replace("<PASSWORD>", DB_PASSWORD);
 
+// MIDDLEWARES:
 app.use(morgan(process.env.NODE_ENV === "development" ? "dev" : "short"));
 
+// SECURITY MIDDLEWARES:
+// 1) Set security HTTP Headers:
+app.use(helmet());
+
+// 2) Limit number of requests in a given time window (in ms) from a certain IP
+app.use(
+  "/",
+  rateLimit({
+    max: process.env.NODE_ENV === "production" ? 300 : 1000,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour",
+  })
+);
+
+// 3) Sanitizing POST request body against NoSQL query injection and XSS:
+app.use(mongoSanitize());
+// app.use(xss());
+
+// 4) Prevent HTTP Parameter Pollution in URL query string (OPTIONAL):
+// app.use(
+//   hpp({
+//     whitelist: ["name", "codeContent" ,"noteContent", "email", "language"],
+//   })
+// );
+
+// APPLICATION MIDDLEWARES:
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -41,6 +72,7 @@ app.use(
   })
 );
 
+// ROUTE MIDDLEWARES:
 app.get("/api/users", async (req, res, next) => {
   try {
     const allUsers = await User.find(req.query);
@@ -50,19 +82,6 @@ app.get("/api/users", async (req, res, next) => {
     res.json({ status: "fail", message: err.message });
   }
 });
-
-// app.post("/users", async (req, res, next) => {
-//   try {
-//     const user = await User.create({
-//       ...req.body,
-//     });
-
-//     res.json(user);
-//   } catch (err) {
-//     console.log(err);
-//     res.json({ status: "fail", message: err.message });
-//   }
-// });
 
 app.use("/api/", authRouter);
 app.use("/api/notes", noteRouter);
